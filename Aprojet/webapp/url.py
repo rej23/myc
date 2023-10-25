ab57d529dbeaa116e6a5c7430986b1bd5e820fb8
rej23 <rejamada@gmail.com>
2023-10-24 23:17:09 -0900

    still working

--- Aprojet/webapp/url.py
+++ Aprojet/webapp/url.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('',views.home, name='home'),
     path('new/', views.new, name='new'),
+    path('thegame/', views.thegame, name='thegame'),
 ]
--- Aprojet/webapp/views.py
+++ Aprojet/webapp/views.py
@@ -4,12 +4,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
-from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib import messages
 from .models import Player, Result
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
+import random
 
+
+import logging
+log = logging.getLogger(__name__)
 # Create your views here.
 # THIS IS THE FIRST FUNCTION def home <CALL THIS URL path('',views.home, name='home'),>
 
@@ -26,13 +29,62 @@ def home(request):
 # WHAT IS THE DIFFERENT BETWEEN USER AND PLAYER
 # THE USER MODEL IS A BUILD-IN MODEL IN DJANGO / USE FOR AUTHENTICATION AND AUTHORIZATION E.G the second if command.
 # WHILE THE PLAYER IS CUSTOM MODEL TO REPRESENT A PLAYER IN A GAME / USE FOR STORING PLAYER INFORMATION EG score.
-        return redirect('new') #THIS CODE IS USE TO REDIRECT TO THE GAME URL PATH. #url.py to set the path
 
+        return redirect('thegame') #THIS CODE IS USE TO REDIRECT TO THE GAME URL PATH. #url.py to set the path
     return render(request, "index.html.html")
 
+def thegame(request):
+
+    possible_actions = ["rock", "paper", "scissors"]
+    computer_action = random.choice(possible_actions)
+    user = Player.objects.all().last() #THIS IS THE RETRIVE THE DETAIL OF THE LAST PLAYER CREATED
 
 
+    if request.method == 'POST':
+        user_action = request.POST.get('name')
+        if computer_action == user_action:
+            messages.info(request, f'IT WAS A TIE TRY AGAIN')
+            result = Result.objects.create(player=user, bot_move=computer_action)
+            log.debug("Both players selected. It's a tie!")
+        elif user_action == "rock":
+            if computer_action == "scissors":
+                messages.success(request,f"Rock smashes scissors! You win!")
+                result = Result.objects.create(player=user, bot_move=computer_action, user_move=user_action, status='Win')
+                log.debug("players selected Rock. Rock smashes scissors! You win!")
+            else:
+                result = Result.objects.create(player=user, bot_move=computer_action, user_move=user_action, status='Lose')
+                messages.info(request, f"Paper covers rock! You lose.")
+                log.debug("paper cover rock. You Lose!")
+
+        elif user_action == "paper":
+            if computer_action == "rock":
+                messages.success(request,f"paper cover rock. You Win!")
+                result = Result.objects.create(player=user, bot_move=computer_action, user_move=user_action, status='Win')
+                log.debug("players selected Paper. paper cover rock. You Win!")
+            else:
+                result = Result.objects.create(player=user, bot_move=computer_action, user_move=user_action, status='Lose')
+                messages.info(request, f"Scissors cuts paper! You lose.")
+                log.debug("Scissors cuts paper! You lose!")
+
+        elif user_action == "scissors":
+            if computer_action == "paper":
+                messages.success(request,f"Scissors cuts paper! You win!")
+                result = Result.objects.create(player=user, bot_move=computer_action, user_move=user_action, status='Win')
+                log.debug("players selected Paper. paper cover rock. You Win!")
+            else:
+                result = Result.objects.create(player=user, bot_move=computer_action, user_move=user_action, status='Lose')
+                messages.info(request, f"Rock smashes scissors! You lose.")
+                log.debug("Rock smashes scissors! You lose.")
+
+
+
+    return render(request, "thegame.html" , {'user':user}) 
 
 def new(request):
+    res = Result.objects.all().order_by('-id')
+    context = {'res' :res}
+    return render(request, "game_record.html", context ) 
+
+
     
-    return render(request, "game_record.html" )
+    
