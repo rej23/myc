8c5b7df38335cd27088ca04a02dcb0c97853e2d9
rej23 <rejamada@gmail.com>
2023-10-26 17:42:37 -0900

    connection to db was successful

--- Aprojet/Aprojet/settings.py
+++ Aprojet/Aprojet/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'Aprojet.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydatabase',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'db',
+        'PORT': '3306',
+
     }
 }
 
